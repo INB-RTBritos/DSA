fea8433c131a5093c10a6586bfc009164f0296d8
INB-RTBritos <154307214+INB-RTBritos@users.noreply.github.com>
2024-05-04 18:05:11 +0800

    commit changes

--- m1Technical.cpp
+++ m1Technical.cpp
@@ -2,19 +2,17 @@
 #include <iomanip>
 using namespace std;
 
-#define MenuSplitter() cout << setw(90) << setfill('-') << " \n\n";
-#define MenuSplitter2() cout << setw(16) << setfill(' ') << " " << setw(90) << setfill('-') << " \n\n";
-
+#define Splitter() cout << setw(90) << setfill('-') << " \n\n";
 
 class Array1D {
-    private:
+    private: 
     int capacity;
     int size;
     int *array;
+
     public:
     Array1D(int ArrayCap=10);
     ~Array1D() {delete [] array;};
-
     int GetSize();
     bool isEmpty() const;
     bool isFull() const;
@@ -37,15 +35,16 @@ int main() {
     arr.AddItem(50);
     arr.RemoveItem(0);
     arr.DisplayElements();
-    
     cout << "Array size: " << arr.GetSize() << endl;
+    if (arr.isEmpty())
+    {
+        cout << "Array is empty!" << endl;
+    }
+    else
+     {
+        cout << "Array is not empty!" << endl;
+    }
 
-    if (arr.isEmpty()) {
-    cout << "Array is empty!" << endl;
-    }
-    else {
-    cout << "Array is not empty!" << endl;
-    }
 
 }
 
@@ -56,67 +55,76 @@ Array1D::Array1D(int ArrayCap) {
 }
 
 int Array1D::GetSize() {
-    return size;
+    int count;
+    for (int i = 0; i > array[capacity]; i++){
+        count++;
+    }
+    return (sizeof(count)*size) / sizeof(int);
 }
 
 bool Array1D::isEmpty() const {
-    if (size == 0)
+    if (size == 0) 
         return true;
-    else
+    else 
         return false;
 }
 
 bool Array1D::isFull() const {
-    if (size == capacity)
+    if (size == capacity) 
         return true;
-    else
+    else 
         return false;
 }
 
 void Array1D::AddItem(int val) {
     if (isFull()) {
-        cout << "Array is full" << endl;
+        cout << "Array is full. Cannot add anymore" << endl;
         return;
     }
 
     array[size] = val;
-    size++;
-}
-
-void Array1D::RemoveItem(int pos) {
-    if (isEmpty()) {
-        cout << "Array is empty" << endl;
-        return;
-    }
-    if (pos < 0 || pos >= size) {
-        cout << "Invalid position" << endl;
-        return;
-    }
-    for (int i = pos; i < size; i++) {
-        array[i] = array[i+1];
-    }
-    size--;
-}
-
-void Array1D::InsertItem(int pos, int val) {
-    if (isFull()) {
-        cout << "Array is full" << endl;
-        return;
-    }
-    if (pos < 0 || pos > size) {
-        cout << "Invalid position" << endl;
-        return;
-    }
-    for (int i = size; i > pos; i--) {
-        array[i] = array[i-1];
-    }
-    array[pos] = val;
-    size++;
+    size = GetSize() + 1;
 }
 
 void Array1D::DisplayElements() {
     for (int i = 0; i < size; i++) {
-        cout << "Element # " << i + 1 << " is " << array[i] << endl << endl;
+        cout << "Element #  " << i + 1 << " : "  << array[i] << endl << endl;
     }
-    MenuSplitter();
+    Splitter();
+}
+
+void Array1D::InsertItem(int pos, int val) {
+    if (isFull()) {
+        cout << "Array is Full. Cannot insert anymore elements" << endl << endl;
+        return;
+    }
+
+    if (pos < 0 || pos > size) {
+        cout << "Invalid position" << endl;
+        return;
+    }
+
+    for (int i = size; i > pos; i--) {
+        array[i] = array[i-1];
+    }
+    array[pos] = val;
+    size = GetSize() + 1;
+}
+
+void Array1D::RemoveItem(int pos) {
+    if (isEmpty()) {
+        cout << "The Array is Currently Empty. Cannot remove any Elements" << endl;
+        return;
+    }
+
+    if (pos < 0 || pos > size) {
+        cout << "The position you want to access does not exist." << endl;
+        return;
+    }
+
+    for (int i = pos; i < size; i++) {
+        array[i] = array[i+1];
+    }
+
+    size = GetSize() - 1;
 }
